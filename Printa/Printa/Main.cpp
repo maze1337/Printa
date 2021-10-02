025cc33e5119da14aa18241cb944f5a210f009ef
Maze <78443372+maze1337@users.noreply.github.com>
2021-10-02 18:09:48 +0200

    Update Main.cpp

--- Printa/Printa/Main.cpp
+++ Printa/Printa/Main.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
 #include <Windows.h>
+#include <string>
 #include "Printa.h"
-
-
 
 
 
 
 int main()
 {
-	//title stuff
-	SetConsoleTitleA("Hacc");
-	Printa.Title("Printa 2.0");
-	
-	//main point of project
-	Printa.OK("Hello World");
-	Printa.Input("Input");
-	Printa.Loading("Loading...");
-	Printa.Fail("Error!");
+    
+    //title stuff
+    SetConsoleTitleA("Hacc");
+    Printa.Title("Printa 2.0");
 
-	// used for input
-	Printa.Arrow(BLUE);
+    //main point of project
+    Printa.OK("Hello World");
+    Printa.Input("Input");
+    Printa.Loading("Loading...");
+    Printa.Fail("Error!");
 
-	// Extra
-	Printa.Endl();
-	Printa.Print("Print Normal Text");
-	Printa.PrintCol("Print Colored Text", YELLOW);
-	Printa.Custom("Custom Symbol Here", BLUE, "Text", GREEN);
-	while (1){}
+    // used for input
+    Printa.Arrow(BLUE);
+
+    // Extra
+    Printa.Endl();
+    Printa.Print("Print Normal Text");
+    Printa.PrintCol("Print Colored Text", LIGHTBLUE);
+    Printa.Custom("(._.)", BLUE, "Text", GREEN);
+    Printa.Endl();
+
+    Printa.Rainbow("Ting and Sus loves enums");
+  
+    while (1) {}
 }
